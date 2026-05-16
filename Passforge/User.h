9b6c35515c041a7ee051865b740e66ef0d8d3d76
azende1231 <azende1231@gmail.com>
2026-05-15 20:16:12 -0400

    Update User.h
    Added user.h code

--- Passforge/User.h
+++ Passforge/User.h
@@ -1,1 +1,32 @@
+#pragma once
+#include <string>
 
+using namespace std;
+
+class User {
+private:
+    string userID;
+    string password;
+    string fname;
+    string lname;
+
+public:
+    User() {
+        userID = "";
+        password = "";
+        fname = "";
+        lname = "";
+    }
+
+    User(string id, string pwd, string f, string l) {
+        userID = id;
+        password = pwd;
+        fname = f;
+        lname = l;
+    }
+
+    string getUserID() { return userID; }
+    string getPassword() { return password; }
+    string getFname() { return fname; }
+    string getLname() { return lname; }
+};
